b42ffbcbd0c4d81843a9ec3479a6cbdbd81ee83c
Maarit Harsu <maarit.harsu@tut.fi>
2020-01-21 13:08:42 +0200

    round 4 published

---
+++ templates/04/game15/board.hh
@@ -0,0 +1,45 @@
+/* Game15 (or puzzle 15) : Template code
+ *
+ * Class: Board
+ *  Describes a two-dimensional grid for puzzle 15 with necessary functions.
+ *
+ * Program author ( Fill with your own info )
+ * Name: Teemu Teekkari
+ * Student number: 123456
+ * UserID: teekkart ( Necessary due to gitlab folder naming. )
+ * E-Mail: teemu.teekkari@tuni.fi
+ *
+ * Notes:
+ *
+ * */
+
+#ifndef BOARD_HH
+#define BOARD_HH
+#include <vector>
+
+const int SIZE = 4;
+
+class Board
+{
+public:
+    // For creating and initializing the grid, you have at least two choices:
+    // 1. Implement a constructor that fills the game grid with empties or such
+    // and two initialize methods
+    // 2. Implement two constructors, one for each initialization way
+
+    // Prints the game grid
+    void print();
+
+    // More methods
+
+private:
+    // Shuffles the numbers vector by using seed as a seed value
+    void my_shuffle(std::vector<unsigned int>& numbers, int seed);
+
+    // Game grid for the 15 puzzle
+    std::vector<std::vector<unsigned int>> grid_;
+
+    // More attributes/methods
+};
+
+#endif // BOARD_HH
---
+++ templates/04/random_numbers/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+void produce_random_numbers(unsigned int lower, unsigned int upper)
+{
+    // Implement your function here
+}
+
+int main()
+{
+    unsigned int lower_bound, upper_bound;
+    cout << "Enter a lower bound: ";
+    cin >> lower_bound;
+    cout << "Enter an upper bound: ";
+    cin >> upper_bound;
+
+    // Reading the line feed, which was left unread by >> operator above.
+    // This is necessary since the seed value will be read by getline
+    // in the function produce_random_numbers.
+    string line_feed = "";
+    getline(cin, line_feed);
+
+    if(lower_bound >= upper_bound)
+    {
+        cout << "The upper bound must be strictly greater than the lower bound" << endl;
+        return EXIT_FAILURE;
+    }
+
+    produce_random_numbers(lower_bound, upper_bound);
+
+    return EXIT_SUCCESS;
+}
