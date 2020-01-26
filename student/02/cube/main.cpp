bb14027ff9820a847468e9e843fcf51720ffb074
Gia Huy Trinh <bdgitr@linux-desktop12.tuni.fi>
2020-01-26 19:53:11 +0200

    2.5.1

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main(){
     int n,area;
-    cout<<"Enter the number: ";
+    cout<<"Enter a number: ";
     cin>>n;
     area=pow(n,3);
     if(area>2147483647 || area<-2147483647 ){
-        cout<<"Error! The cube of 100000 is not "<<area<<endl;
+        cout<<"Error! The cube of "<<n<<" is not "<<area<<endl;
     }
     else{
         cout<<"The cube of "<<n<<" is "<<area<<"."<<endl;
