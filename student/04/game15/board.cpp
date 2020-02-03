4f0746200bb6564391ad7773bf0d24ccff815e24
Gia Huy Trinh <bdgitr@linux-desktop9.tuni.fi>
2020-02-03 02:53:46 +0200

    4.3.1

---
+++ student/04/game15/board.cpp
@@ -0,0 +1,56 @@
+/* Game15 (or puzzle 15) : Template code
+ *
+ * Class: Board
+ *
+ * Program author ( Fill with your own info )
+ * Name: Teemu Teekkari
+ * Student number: 123456
+ * UserID: teekkart ( Necessary due to gitlab folder naming. )
+ * E-Mail: teemu.teekkari@tuni.fi
+ *
+ * Notes:
+ *
+ * */
+
+#include "board.hh"
+#include <iostream>
+#include <iomanip>
+
+const int EMPTY = 16;
+const unsigned int PRINT_WIDTH = 5;
+
+void Board::print()
+{
+    for(unsigned int x = 0; x < SIZE; ++x)
+    {
+        std::cout << std::string(PRINT_WIDTH * SIZE + 1, '-') << std::endl;
+        for(unsigned int y = 0; y < SIZE; ++y)
+        {
+            std::cout << "|" << std::setw(PRINT_WIDTH - 1);
+            if(grid_.at(x).at(y) != EMPTY)
+            {
+                std::cout << grid_.at(x).at(y);
+            }
+            else
+            {
+                std::cout << ".";
+            }
+        }
+        std::cout << "|" << std::endl;
+    }
+    std::cout << std::string(PRINT_WIDTH * SIZE + 1, '-') << std::endl;
+}
+
+void Board::my_shuffle(std::vector<unsigned int> &numbers, int seed)
+{
+    std::default_random_engine randomEng(seed);
+    std::uniform_int_distribution<int> distr(0, numbers.size() - 1);
+    for(unsigned int i = 0; i < numbers.size(); ++i)
+    {
+        unsigned int random_index = distr(randomEng);
+        unsigned int temp = numbers.at(i);
+        numbers.at(i) = numbers.at(random_index);
+        numbers.at(random_index) = temp;
+    }
+}
+
---
+++ student/04/game15/main.cpp
@@ -0,0 +1,31 @@
+/* Game15 (or puzzle 15) : Template code
+ *
+ * Desc:
+ *  This program generates a 15 puzzle. The board is SIZE x SIZE,
+ * (original was 4x4) and every round the player chooses a direction and
+ * a number tile, making the given tile to be moved to the given direction.
+ *  Game will end when the numbers 1-15 (1-16) are in the ascending order,
+ * starting from top left and proceeding row by row.
+ * Empty space is represented by the number 16 in the code (constant EMPTY)
+ * and by dot when printing the game board.
+ *
+ * Program author ( Fill with your own info )
+ * Name: Teemu Teekkari
+ * Student number: 123456
+ * UserID: teekkart ( Necessary due to gitlab folder naming. )
+ * E-Mail: teemu.teekkari@tuni.fi
+ *
+ * Notes about the program and it's implementation:
+ *
+ * */
+
+#include "board.hh"
+
+// More functions
+
+int main()
+{
+    // More functionality
+
+    return EXIT_SUCCESS;
+}
