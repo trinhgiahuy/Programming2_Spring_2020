5a196c388b9bfdad85a954072e71c7649482a7b9
Gia Huy Trinh <bdgitr@linux-desktop12.tuni.fi>
2020-02-16 22:15:28 +0200

    5.5.1

--- student/05/points/main.cpp
+++ student/05/points/main.cpp
@@ -1,74 +1,56 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <map>
-#include <set>
 #include <string>
+#include <map>
 
 using namespace std;
 
 int main()
 {
     string filename_input = "";
-
     cout << "Input file: ";
     getline(cin, filename_input);
 
-    fstream file_in(filename_input);
+    ifstream file_in(filename_input);
 
     if (file_in)
     {
-        map<string, set<int>> wordcount;
-
-        int line_number = 1;
+        map<string, int> player_point;
         string line = "";
+
         while (getline(file_in, line))
         {
-            string::size_type i = 0;
-            string::size_type space_position = 0;
-            while (space_position != string::npos)
+            string::size_type colon_position = line.find(':');
+            string name;
+            name = line.substr(0, colon_position);
+            int points;
+            points = stoi(line.substr(colon_position + 1, line.length() - colon_position - 1));
+
+            if (player_point.find(name) != player_point.end())
             {
-                space_position = line.find(' ', i);
-                string word = line.substr(i, space_position - i);
-                if (wordcount.find(word) != wordcount.end())
-                {
-                    wordcount.at(word).insert(line_number);
-                }
-                else
-                {
-                    set<int> line_list = {line_number};
-                    wordcount.insert({word, line_list});
-                }
-
-                i = space_position + 1;
+                player_point.at(name) += points;
+            } else
+            {
+                player_point.insert({name, points});
             }
-
-
-            line_number++;
 
         }
 
-        for (auto data : wordcount)
+        file_in.close();
+
+        cout << "Final scores:\n";
+        for (auto data : player_point)
         {
-            cout << data.first << " " << data.second.size() << ": ";
-            for (set<int>::iterator i = data.second.begin(); i != data.second.end(); i++)
-            {
-                if (i != data.second.begin())
-                {
-                    cout << ", ";
-                }
-
-                cout << *i;
-            }
-
-            cout << "\n";
+            cout << data.first << ": " << data.second << "\n";
         }
+
     }
     else
     {
         cout << "Error! The file " << filename_input << " cannot be opened.";
         return EXIT_FAILURE;
     }
+
 
     return EXIT_SUCCESS;
 }
